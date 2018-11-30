6a8b388d01e5d9590aabb6fba5368a9f17747717
Brendan <brendanekane@gmail.com>
2018-11-29 20:03:23 -0500

    number swap solution in js and ruby

---
+++ cracking_coding_interview/chap_16/number_swap.js
@@ -0,0 +1,20 @@
+/*
+                DIFF
+|----------|----------------|
+0          A(0)            B(0)
+
+DIFF = A(0) - B(0) || B(0) - A(0)
+
+If we assign A to the difference between the two numbers
+Then we assign B to the sum of itself and DIFF B will now equal A(0)
+Then we can assign A to the difference of B and DIFF (which will make it
+equal to B(0))
+
+*/
+
+const numSwap = (x,y) => {
+  x = x - y;
+  y = y + x;
+  x = y - x;
+  return [x,y];
+};
